2af36e40bacc82909d1e95965ddc0952561d2fff
hab278 <hab278@nyu.edu>
2015-01-30 17:36:58 -0500

    Fix error with adding renderers

--- lib/citero_renderers.rb
+++ lib/citero_renderers.rb
@@ -7,15 +7,17 @@ module CiteroRenderers
       require 'citero_renderers/mime_types/bibtex_mime_type'
       require 'action_controller/metal/renderers'
       # Adds a renderer for BibTeX.
-      ActionController.add_renderer :bibtex do |bibtex|
+      require 'pry'
+      # binding.pry
+      ActionController.add_renderer :bibtex do |bibtex,option|
         bibtex.extend CiteroRenderers::BibtexRenderable unless bibtex.respond_to?(:to_bibtex)
         filename = (bibtex.respond_to?(:to_param) and bibtex.class.respond_to?(:acts_as_citable)) ? bibtex.to_param : "export"
         bibtex = bibtex.to_bibtex
         send_data bibtex, type: Mime::Type.lookup(:bibtex), disposition: "attachment; filename=#{filename}.bib"
       end
       # Adds a renderer for RIS.
-      ActionController.add_renderer :ris do |ris|
-        ris.extend CiteroRenderers::RisRenderable unless bibtex.respond_to?(:to_ris)
+      ActionController.add_renderer :ris do |ris, option|
+        ris.extend CiteroRenderers::RisRenderable unless ris.respond_to?(:to_ris)
         filename = (ris.respond_to?(:to_param) and ris.class.respond_to?(:acts_as_citable)) ? ris.to_param : "export"
         ris = ris.to_ris()
         send_data ris, type: Mime::Type.lookup(:ris), disposition: "attachment; filename=#{filename}.ris"
